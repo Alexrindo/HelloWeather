d31c40d660bcfcdccb7e77669c8376b771eea632
= <=>
2016-12-27 23:21:13 +0100

    Updated examples

--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Examples = () => {
   	return (
   		<div>
-  		<h3>Examples Component!</h3>
+  		<h3>Examples</h3>
   		<p>Welcome to Example page!</p>
   		</div>
   		)
